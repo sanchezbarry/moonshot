6934f3ef1abe8e55aa41e6b1b5edd5758b7f552e
Sanchez <sanchezbarry@gmail.com>
2026-05-13 11:58:57 +0800

    changed acme to moonshot

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,7 +10,7 @@ export default function SignupPage() {
             <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEndIcon className="size-4" />
             </div>
-            Acme Inc.
+            moonshot
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
